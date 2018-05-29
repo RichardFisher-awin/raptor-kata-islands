4e94359deacaa7a214f59ad7bbf5ec182dc68f3f
Richard Fisher <richard.fisher@affiliatewindow.com>
2018-05-29 15:50:22 +0100

    RF - OOing it to death

---
+++ spec/MapSpec.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace spec\Kata;
+
+use Kata\Area;
+use Kata\Map;
+use PhpSpec\ObjectBehavior;
+
+class MapSpec extends ObjectBehavior
+{
+    function let()
+    {
+        $this->beConstructedWith([
+           [1, 0, 0],
+           [0, 1, 1]
+        ]);
+
+        $this->shouldHaveType(Map::class);
+    }
+
+    function it_can_return_the_area_at_coordinates()
+    {
+        $this->getArea(0, 0)->shouldBeLike(new Area(1, 0, 0, $this->getWrappedObject()));
+    }
+
+    function it_can_return_a_flattened_list()
+    {
+        $this->getFlattenedList()->shouldHaveCount(6);
+    }
+}
--- src/Service/IslandCounter.php
+++ src/Service/IslandCounter.php
@@ -7,8 +7,34 @@ class IslandCounter
 {
     public function countIslands(array $data) : int
     {
-        // @todo implementation
-        return 0;
+        $map = new Map($data);
+        $count = 0;
+
+        /** @var Area $area */
+        foreach ($map->getFlattenedList() as $area) {
+            if ($area->isLand()) {
+                $count ++;
+                $this->sinkIsland($area);
+            }
+        }
+
+        return $count;
     }
 
+    private function sinkIsland(Area $area)
+    {
+        $candidates = [$area];
+
+        while (! empty($candidates)) {
+            /** @var Area $candidate */
+            $candidate = array_shift($candidates);
+            $candidate->sink();
+            /** @var Area $neighbour */
+            foreach ($candidate->getNeighbours() as $neighbour) {
+                if ($neighbour->isLand()) {
+                    $candidates[] = $neighbour;
+                }
+            }
+        }
+    }
 }
