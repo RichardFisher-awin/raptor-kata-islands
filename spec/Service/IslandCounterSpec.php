36724034f83924c4b68a73f7d1d4f52c89cf471a
Bhavesh.mistry <bhavesh.mistry@digitalwindow.com>
2018-05-29 15:21:04 +0100

    test island

--- spec/Service/IslandCounterSpec.php
+++ spec/Service/IslandCounterSpec.php
@@ -51,4 +51,74 @@ class IslandCounterSpec extends ObjectBehavior
         ];
         $this->countIslands($map)->shouldBe(3);
     }
+
+  function it_can_count_example_five()
+    {
+        $map = [
+            [1, 1, 1, 1, 1],
+            [1, 0, 0, 0, 1],
+            [1, 0, 1, 0, 1],
+            [1, 0, 0, 0, 1],
+	    [1, 1, 1, 1, 1]
+        ];
+        $this->countIslands($map)->shouldBe(2);
+    }
+
+  function it_can_count_example_six()
+    {
+        $map = [
+            [1, 0, 1],
+            [0, 1, 0],
+            [1, 0, 1]
+        ];
+        $this->countIslands($map)->shouldBe(5);
+    }
+
+  function it_can_count_example_seven()
+    {
+        $map = [
+            [1, 0, 0, 0, 0],
+            [1, 1, 0, 0, 0],
+            [1, 1, 1, 0, 1],
+            [0, 0, 0, 1, 1],
+            [0, 0, 1, 1, 1]
+        ];
+        $this->countIslands($map)->shouldBe(2);
+    }
+
+function it_can_count_example_eight()
+    {
+        $map = [
+            [0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1],
+            [0, 0, 0, 0, 0],
+            [1, 1, 0, 1, 1],
+            [0, 0, 0, 0, 0]
+        ];
+        $this->countIslands($map)->shouldBe(3);
+    }
+
+function it_can_count_example_nine()
+    {
+        $map = [
+            [1, 1, 1, 1, 1],
+            [1, 1, 0, 1, 1],
+            [1, 0, 0, 0, 1],
+            [1, 1, 0, 1, 1],
+            [1, 1, 1, 1, 1]
+        ];
+        $this->countIslands($map)->shouldBe(1);
+    }
+
+function it_can_count_example_ten()
+    {
+        $map = [
+            [0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1],
+            [0, 1, 0, 1, 0],
+            [1, 1, 0, 1, 1],
+            [1, 0, 0, 0, 1]
+        ];
+        $this->countIslands($map)->shouldBe(1);
+    }
 }
