ba90b8d8817bf907eb7a0f60636c4d73e5c7b0db
Richard Fisher <richard.fisher@affiliatewindow.com>
2018-05-29 15:47:49 +0100

    Silly approach, using image magick

--- src/Service/IslandCounter.php
+++ src/Service/IslandCounter.php
@@ -1,14 +1,44 @@
 <?php
 
 namespace Kata\Service;
-use Kata\{Map, Area};
 
 class IslandCounter
 {
     public function countIslands(array $data) : int
     {
-        // @todo implementation
-        return 0;
+        $y = count($data);
+        $x = count($data[0]);
+
+        $im = new \Imagick();
+        $im->newImage($x, $y, new \ImagickPixel('black'));
+
+
+        //create the image (set '1' pixels to white)
+        $it = $im->getPixelIterator();
+
+        foreach ($it as $yi => $row) {
+            foreach ($row as $xi => $pixel) {
+                if ($data[$yi][$xi] == 1) {
+                    $pixel->setColor('white');
+                    $it->syncIterator();
+                }
+            }
+        }
+
+        $flood = function($im, $count = 0) use (&$flood) {
+            $it = $im->getPixelIterator();
+            foreach ($it as $yi => $row) {
+                foreach ($row as $xi => $pixel) {
+                    if ($pixel->getColor()['r']) {
+                        $im->floodfillPaintImage("black", 0, 'white', $xi, $yi, false);
+                        return $flood($im, $count + 1);
+                    }
+                }
+            }
+            return $count;
+        };
+
+        return $flood($im);
     }
 
 }
